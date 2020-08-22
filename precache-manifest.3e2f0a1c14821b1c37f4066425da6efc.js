dfdcfce49b0c6df44561935e2dfed091e1821ad8
asadalibest1 <asadalibest1@gmail.com>
2020-08-22 17:04:36 +0500

    Updates

---
+++ precache-manifest.3e2f0a1c14821b1c37f4066425da6efc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e896efbf09e6c39c8ca354b8e59c108d",
+    "url": "/webflex-project/index.html"
+  },
+  {
+    "revision": "83cd62c16886bcc0cc98",
+    "url": "/webflex-project/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "7c4c54ac8b462639e9b4",
+    "url": "/webflex-project/static/css/main.9daf01ae.chunk.css"
+  },
+  {
+    "revision": "83cd62c16886bcc0cc98",
+    "url": "/webflex-project/static/js/2.f9f66e86.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/webflex-project/static/js/2.f9f66e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4c54ac8b462639e9b4",
+    "url": "/webflex-project/static/js/main.7c44caa9.chunk.js"
+  },
+  {
+    "revision": "73fed02bdb057204b015",
+    "url": "/webflex-project/static/js/runtime-main.93b05924.js"
+  },
+  {
+    "revision": "94486e8b622bd66f541c2fcddcba2669",
+    "url": "/webflex-project/static/media/birdwall.94486e8b.jpg"
+  },
+  {
+    "revision": "d8b3e1dc1af42e0c47660e7077add54f",
+    "url": "/webflex-project/static/media/image1.d8b3e1dc.gif"
+  },
+  {
+    "revision": "af3aff654f3088793c4ba0b196c249fa",
+    "url": "/webflex-project/static/media/image2.af3aff65.png"
+  },
+  {
+    "revision": "8c2acb4e5c4d6488c218e4ce33976d79",
+    "url": "/webflex-project/static/media/react.8c2acb4e.png"
+  }
+]);
